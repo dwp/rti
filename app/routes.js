3572abe677bbb68ffbc5cb3f7aaf1b5d1943652a
Gup <gurpreetbadyal@hotmail.com>
2014-12-18 09:49:07 +0000

    First Commit - heroic deployment

---
+++ app/routes.js
@@ -0,0 +1,33 @@
+module.exports = {
+  bind : function (app, assetPath) {
+    app.get('/', function (req, res) {
+      res.render('index',
+                {'assetPath' : assetPath});
+    });
+
+    /* Example pages */
+
+    app.get('/examples/hello-world', function (req, res) {
+      res.render('examples/hello-world', {'message' : 'Hello world'});
+    });
+
+    app.get('/examples/inheritance', function (req, res) {
+      res.render('examples/inheritance/page-level', {'message' : 'Hello world'});
+    });
+
+    app.get('/examples/alpha', function (req, res) {
+      res.render('examples/alpha/alpha', {'assetPath' : assetPath });
+    });
+
+    app.get('/examples/elements', function (req, res) {
+      res.render('examples/elements/forms', {'assetPath' : assetPath });
+    });
+
+    app.get('/examples/rti', function (req, res) {
+      res.render('examples/rti/rti', {
+        'assetPath' : assetPath,
+        'name'      : 'Gurpreet'
+        });
+    });
+  }
+};
